905ff251b6a860be734e783ab7823953bf477d97
nimiCurtis <nimicu21@gmail.com>
2022-11-24 19:55:00 +0200

    upadte sync and data structure

--- env_recorder_pkg/scripts/modules/bag_reader/bag_reader.py
+++ env_recorder_pkg/scripts/modules/bag_reader/bag_reader.py
@@ -37,12 +37,8 @@ class BagReader():
         else:
             self.MetaData["exported"] = False
         
-        # initialize data frames
-        self.rgb_df = pd.DataFrame()
-        self.depth_df = pd.DataFrame()
-        self.imu_df = pd.DataFrame()
-        self.confidence_df = pd.DataFrame()
-        self.disparity_df = pd.DataFrame()
+        # initialize data frames dictionary
+        self.dfs = {}
 
         # initialize counter
         self.frame_count = 0
@@ -61,21 +57,12 @@ class BagReader():
             """        
 
         print("[INFO]  Bag already exported, Reading data ...")
-
-        if "imu" in self.MetaData:
-            self.imu_df = pd.read_csv(self.MetaData["imu"])
-        if "rgb" in self.MetaData:
-            self.rgb_df = pd.read_csv(self.MetaData["rgb"])
-        if "depth" in self.MetaData:
-            self.depth_df = pd.read_csv(self.MetaData["depth"])
-        if "confidence" in self.MetaData:
-            self.confidence_df = pd.read_csv(self.MetaData["confidence"])
-        if "disparity" in self.MetaData:
-            self.disparity_df = pd.read_csv(self.MetaData["disparity"])
-        if "pointclod" in self.MetaData:
-            self.pointclod_df = pd.read_csv(self.MetaData["pointclod"])
-        if "tf" in self.MetaData:
-            self.tf_df = pd.read_csv(self.MetaData["tf"])
+        
+        names = ["imu","rgb","depth","confidence","disparity","pointclod","tf","synced"] # change based on configuration
+        
+        for name in names:
+            if name in self.MetaData:
+                self.dfs[name] = pd.read_csv(self.MetaData[name])
 
     def export(self):
         """This function export the data from the bag and modify the data frames accordingly
@@ -85,21 +72,26 @@ class BagReader():
         print("[INFO]  Bag doesn't exported, Exporting data ...")
 
         # set topic df
-        self.topic_df = self.bag_read.topic_table
+        self.topic_df = self.bag_read.topic_table 
         
         # read and set imu_df
         for index, topic_row in self.topic_df.iterrows():
             if (topic_row['Types']=='sensor_msgs/Imu') and  topic_row['Message Count']!=0: # stop when topic is the imu topic and its not empty
                     imu_file = self.bag_read.message_by_topic(topic_row['Topics']) # create the csv file  
-                    self.imu_df = pd.read_csv(imu_file) # set the df
-                    self.imu_df.drop_duplicates(subset=['header.stamp.secs','header.stamp.nsecs'],ignore_index=True,inplace=True)
-                    self.imu_df.to_csv(imu_file) # rewrite imu csv
+                    self.dfs["imu"] = pd.read_csv(imu_file) # set the df
+                    self.dfs["imu"].drop_duplicates(subset=['header.stamp.secs','header.stamp.nsecs'],ignore_index=True,inplace=True)
+                    self.dfs["imu"].to_csv(imu_file) # rewrite imu csv
                     self.MetaData["imu"] = imu_file # save the path to metadata
-#df = pd.merge(bag_obj.imu_df,bag_obj.depth_df[["header.stamp.secs","header.stamp.nsecs","np_path","frame_path"]],on=["header.stamp.secs","header.stamp.nsecs"],how="right")
 
             if ((topic_row['Types']=='sensor_msgs/Image')or(topic_row['Types']=='stereo_msgs/DisparityImage')) and  topic_row['Message Count']!=0: # stop when topic is Image/Stereo kind and its not empty
                 self.init_image_df(topic_row['Topics']) 
         
+        self.dfs["synced"] = self.sync_imu_with([v for k, v in self.dfs.items() if k!='imu'])
+        synced_file = os.path.join(self.bag_read.datafolder,"synced.csv")
+        self.dfs["synced"].to_csv(synced_file)
+        self.MetaData["synced"] = synced_file  
+
+        # save the path to metadata
         # change exported status and dump MetaData to a json file
         self.MetaData["exported"] = True
         with open(self.metadata_file, "w") as json_file:
@@ -114,21 +106,12 @@ class BagReader():
 
         topic_split = topic.split('/')
 
-        # handle with depth topic
-        if 'depth' in topic_split:
-            self.depth_df = self.set_image_df(topic,'depth')
+        names = ["rgb","depth","confidence","disparity","pointclod"] # change based on configuration
 
-        # handle with rgb topic
-        if 'rgb' in topic_split: 
-            self.rgb_df = self.set_image_df(topic,'rgb')
-        
-        # handle with confidence
-        if 'confidence' in topic_split:
-            self.confidence_df = self.set_image_df(topic,'confidence')
-
-        # handle with disaprity
-        if 'disparity' in topic_split:
-            self.disparity_df = self.set_image_df(topic, 'disparity')
+        for name in names:
+            if name in topic_split:
+                self.dfs[name] = self.set_image_df(topic,name)
+                break
 
     def set_image_df(self,topic,img_type):
         """ This function creating the image type dataframes and csv files including the outputs from 
@@ -151,21 +134,17 @@ class BagReader():
         # crating data folder for the relevant image type
         if not os.path.exists(dir):
             os.mkdir(dir)
-            if not os.path.exists(dir_vals) and img_type != 'rgb': # if not rgb type create values directory
+            if not os.path.exists(dir_vals) : # create values directory
                 os.mkdir(dir_vals) 
         
         # create csv temp file using the bagreader library - temporarily because it deosnt handle good with imgs 
         tmp_file = self.bag_read.message_by_topic(topic)
         df = pd.read_csv(tmp_file)
         if (img_type == 'disparity'):
-            df.columns = df.columns.str.replace('image.','') # rename disparity columns starting with 'image.'
+            df.columns = df.columns.str.replace('image.data','data',regex=True) # rename disparity columns starting with 'image.'
         df.drop('data',inplace = True , axis =1) # drop the data column because its containing garbage data
 
-
-        if img_type == 'rgb' : # if rgb save only frame paths
-            df['frame_path'], _ = self.extract_images(topic, dir, img_type)
-        else:
-            df['frame_path'], df['np_path'] = self.extract_images(topic, dir, img_type)
+        df['frame_path'], df['np_path'] = self.extract_images(topic, dir, img_type)
 
         df.to_csv(tmp_file) # create updated csv file
         self.MetaData[img_type] = tmp_file # save to metadata
@@ -208,7 +187,8 @@ class BagReader():
 
             if (img_type != "rgb"): # if not rgb type save the real values as .npy file, and update np list
                 values_array = np.array(cv_img, dtype=np.float32) # convert type to d_type --> extract depth values
-                
+            
+
                 if (img_type == "depth"):
                     #values_array = values_array/1000.0 # convert to meters
                     cv_img = self.get_depth_normalization(values_array) # get normalized image of depth values
@@ -216,10 +196,10 @@ class BagReader():
                 if (img_type == "disparity"):
                     cv_img = self.get_disparity_colormap(values_array,msg) # get color map of dispatity values
                 
-                
-                numpy_path_list.append(self.save_np_data(values_array,dir)) # save values
-                
+            else:
+                values_array = np.array(cv_img, dtype=np.int32)
 
+            numpy_path_list.append(self.save_np_data(values_array,dir)) # save values
             frame_path_list.append(frame_path) # update frame path list
             cv2.imwrite(frame_path, cv_img)    # save img
 
@@ -280,14 +260,15 @@ class BagReader():
 
         return cv2.cvtColor(colormap_disparity, cv2.COLOR_BGR2RGB)                # convert to rgb --> red = close dist, blue = far dist
 
-    def get_synced_df(self,dfs):
+    def sync_imu_with(self,dfs):
         cols = ['np_path', 'frame_path']
-        df_sync = dfs[0]
-        for df in dfs[1:]:
-            key = [k for k, v in dic.items() if v == df]
-            df = df.rename(columns={c: c+f'_{key}' for c in df.columns if c in cols})
-            df_sync = pd.merge(df_sync,df[["header.stamp.secs","header.stamp.nsecs",f"np_path_{key}",f"frame_path_{key}"]],on=["header.stamp.secs","header.stamp.nsecs"],how="right")    
-        return df
+        df_sync = self.dfs["imu"].copy()
+        for df in dfs:
+            key = [k for k, v in self.dfs.items() if v.equals(df)]
+            df = df.rename(columns={c: c+f'_{key[0]}' for c in df.columns if c in cols})
+            df_sync = pd.merge(df_sync,df[["header.stamp.secs","header.stamp.nsecs",f"np_path_{key[0]}",f"frame_path_{key[0]}"]],on=["header.stamp.secs","header.stamp.nsecs"],how="right")    
+        
+        return df_sync
 
 if __name__ == '__main__':
     bag_obj = BagReader('/home/nimibot/catkin_ws/src/ros_env_prediction/env_recorder_pkg/bag/2022-11-08-10-07-30.bag')
